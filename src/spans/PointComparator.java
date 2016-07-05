efafc082a0285b73d2363eccc9fd5129256b7dab
GeePawHill <geepawhill@geepawhill.org>
2016-07-05 04:17:05 -0400

    COmment!?!?!? for clarity.

--- src/spans/PointComparator.java
+++ src/spans/PointComparator.java
@@ -10,19 +10,20 @@ public class PointComparator implements Comparator<Point>
 	{
 		if (lhs.value > rhs.value) return 1;
 		if (lhs.value < rhs.value) return -1;
-		if(lhs.type==rhs.type) return 0;
-		switch(lhs.type)
+		if (lhs.type == rhs.type) return 0;
+		switch (lhs.type)
 		{
 		case LT:
 		case LE:
-			if(lhs.includes(rhs)) return 1;
+			// with lessers the includer is the smallest
+			if (lhs.includes(rhs)) return 1;
 			return -1;
 		case GT:
 		case GE:
-			if(lhs.includes(rhs)) return -1;
+			// with greaters the includer is the largest 
+			if (lhs.includes(rhs)) return -1;
 			return 1;
 		}
-//		if (lhs.includes(rhs)) return -1;
-		return 1;
+		return 0;
 	}
 }
